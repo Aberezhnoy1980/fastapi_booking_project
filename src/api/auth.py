601cf9b7c7cd83e1dadb730169b483456ca60768
Aleksandr Berezhnoy <alex@iMac-IMAC.local>
2025-03-12 17:43:37 +0300

    added logout api

--- src/api/auth.py
+++ src/api/auth.py
@@ -50,6 +50,14 @@ async def login_user(
         return {"access_token": access_token}
 
 
+@router.post("/logout")
+async def logout(
+        response: Response
+):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
+
+
 @router.get(
     "/me",
     summary="Получение данных текущего пользователя",
